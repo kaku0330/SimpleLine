2e42d2d0fc935fbb0ce452dbc25eabedc2264158
kaku0330 <tmpss94812@gmail.com>
2024-01-27 12:14:52 +0800

    try composeup successfully

--- testDrf/testDrfapp/models.py
+++ testDrf/testDrfapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
 class testDrf(models.Model):
     test_id = models.CharField(db_column="test_id",unique= True,max_length=128)
     test_name = models.CharField(db_column='test_name',unique= False,max_length=10)
