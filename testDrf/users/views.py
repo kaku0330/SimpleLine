b5f976f16e78d3c3d80078e2ba04b0f7d2ab5164
kaku0330 <tmpss94812@gmail.com>
2024-03-06 21:48:18 +0800

    setup channels & change gunicorn to uvicorn & add usergroupapp

--- testDrf/users/views.py
+++ testDrf/users/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializer import CreateUserModelSerializers
-from .models import user
+from .models import User
 # Create your views here.
 
 class UsersViewSet(viewsets.ModelViewSet):
-    queryset = user.objects.all()
+    queryset = User.objects.all()
     serializer_class = CreateUserModelSerializers
 
     # def get_serializer_class(self):
